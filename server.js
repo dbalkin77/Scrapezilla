b400eaede9951b9c31385f56c06fdb68775820db
Douglas Balkin <dbalkin77@gmail.com>
2017-09-16 10:53:27 -0500

    More setup

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const cheerio = require("cheerio");
 const request = require("request");
+const express = require("express");
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+
+// Set mongoose to leverage built in JavaScript ES6b Promises
+mongoose.Promise = Promise;
+
+const app = express();
