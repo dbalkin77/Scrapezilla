2683c6afb92b2662bbf83eac2cbf70eead22d5b9
Douglas Balkin <dbalkin77@gmail.com>
2017-09-18 13:19:12 -0500

    updates to Article.js

--- models/Article.js
+++ models/Article.js
@@ -2,3 +2,23 @@
 const mongoose = require("mongoose");
 // Create Schema class
 const Schema = mongoose.Schema;
+
+// Create Article Schema
+var articleSchema = new Schema({
+    headline: {
+        type: String,
+        required: true
+    },
+    sumary: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
